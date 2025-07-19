5b7a5fd271f556950e1e53d7198fe78415c7efbd
Armando <125172812+armando-2002@users.noreply.github.com>
2025-07-19 15:11:20 -0500

    Agregar metodos en reservas,pedidio y mesa

--- backend/src/routes/reserva-rt.js
+++ backend/src/routes/reserva-rt.js
@@ -26,7 +26,37 @@ router.post("/", async (req, res) => {
     res.status(500).json({ error: "Error al crear la reserva", details: error });
   }
 });
+// PUT - Actualizar reserva por ID
+router.put("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { id_cliente, id_mesa, fecha, num_personas, estado } = req.body;
+  try {
+    const reserva = await prisma.reserva.update({
+      where: { id},
+      data: {
+        id_cliente,
+        id_mesa,
+        fecha: new Date(fecha),
+        num_personas,
+        estado,
+      },
+    });
+    res.json(reserva);
+  } catch (error) {
+    res.status(400).json({ error: "No se pudo actualizar la reserva.", detalle: error.message });
+  }
+});
 
+// DELETE - Eliminar reserva por ID
+router.delete("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    await prisma.reserva.delete({ where: { id } });
+    res.json({ message: "Reserva eliminada" });
+  } catch (error) {
+    res.status(500).json({ error: "Error al eliminar la reserva" });
+  }
+});
 export default router;
 
 //put
