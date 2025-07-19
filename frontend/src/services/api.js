cd33b79c1a319fc0ce9738b25f2057c4fd04a3d9
Roberth <94004643+R0berth456@users.noreply.github.com>
2025-07-19 15:29:14 -0500

    Delete frontend directory

--- frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-const API_BASE_URL = 'http://localhost:3000'; // Asegúrate de usar el puerto correcto
-
-export const fetchMenu = async () => {
-  const res = await fetch(`${API_BASE_URL}/menu`);
-  if (!res.ok) throw new Error('Error al obtener el menú');
-  return await res.json();
-};
